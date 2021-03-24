7a239ab5abca07a69c5b35301481a0fbfa8b1c25
zakaria <zakariaselassi@gmail.com>
2021-03-24 10:18:58 +0100

    update submit & blind

--- index.php
+++ index.php
@@ -1,75 +1,85 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <title>Title</title>
     <!-- Required meta tags -->
-    <meta name= "description" content= "formulaire " />
+    <meta name="description" content="formulaire " />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/style/style.css">
-  </head>
+</head>
+
 <body>
     <?php include 'country.php'?>
     <?php include 'verification.php'?>
-<main>
-    <div class="contact">
-        <h1>Contact US</h1>
-        <img src="assets/img/logo.png">
-    </div>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-      <div class="form-group">
-        <label for="name">Name:</label>
-        <input type="text" class="form-control  transparent-input" name="name" id="name">
-        <input type="text" name="honey" id="honeyPot">
-        <p class="error"><?php echo $nameError?></p>
-      </div>
-      <div class="form-group">
-        <label for="lastname">lastName:</label>
-        <input type="text" class="form-control  transparent-input" name="lastName" id="lastname">
-        <p class="error"><?php echo $lastNameError?></p>
-      </div>
-      <div>
-        <label for="gender">Gender :</label>
-        <div class="form-check-inline">
-            <label class="form-check-label">
-              <input type="radio" class="form-check-input " name="gender" value="Male"> MALE
-              <input type="radio" class="form-check-input" name="gender" value="female"> FEMALE
-            </label>
-            <p class="error"><?php echo $genderError?></p>
+    <main>
+        <div class="contact" aria-labelledby="title">
+            <h1 id="title">Contact US</h1>
+            <img src="assets/img/logo.png" alt="pirate logo">
         </div>
-      </div>
-      <div class="form-group">
-        <label for="email">email :</label>
-        <input type="email" class="form-control  transparent-input" name="email" id="email">
-        <p class="error"><?php echo $emailError?></p>
-      </div>
-      <div class="form-group">
-        <select class="form-control form-control-sm" name="country" id="ctry">
-        <?php
-            foreach($country as $key => $value)
-          {
-              echo '<option value="'.$key.'">'.$value.'</option>';
-            }
-        ?>
-        </select>
-      </div>
+        <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>" method="POST">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control  transparent-input" name="name" id="name" aria-labelledby="name">
+                <input type="text" name="honey" id="honeyPot">
+                <p class="error">
+                    <?php echo $nameError?>
+                </p>
+            </div>
+            <div class="form-group">
+                <label for="lastname">lastName:</label>
+                <input type="text" class="form-control  transparent-input" name="lastName" id="lastname" aria-labelledby="lastname">
+                <p class="error">
+                    <?php echo $lastNameError?>
+                </p>
+            </div>
+            <div>
+            <div class="form-check-inline">
+                <label for="gender">Gender :</label>
+                        <input type="radio" class="form-check-input " name="gender" value="Male"> MALE
+                        <input type="radio" class="form-check-input" name="gender" value="female"> FEMALE
+                    </label>
+                    <p class="error">
+                        <?php echo $genderError?>
+                    </p>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="email">email :</label>
+                 <input type="email" class="form-control  transparent-input" name="email" id="email" aria-labelledby="email">
+                    <p class="error">
+                        <?php echo $emailError?>
+                    </p>
+            </div>
+            <div class="form-group">
+                <select class="form-control form-control-sm" name="country" id="ctry" aria-labelledby="ctry">
+                <?php
+                    foreach($country as $key => $value)
+                {
+                    echo '<option value="'.$key.'">'.$value.'</option>';
+                    }
+                ?>
+                </select>
+            </div>
 
-      <div class="form-group">
-        <select class="form-control form-control-sm" name="subject" id="subject">
-            <option>Question</option>
-            <option>complaint</option>
-            <option>Other</option>
-        </select>
-      </div>
-      <div class="form-group shadow-textarea  ">
-        <textarea class="form-control z-depth-1 transparent-input" id="textArea" rows="3" placeholder="Write something here..." name="comment"></textarea>
-        <p class="error"></p>
-      </div>
-      <button type="submit" class="btn btn-primary" >Submit</button>
-    </form>
-</main>
+            <div class="form-group">
+                <select class="form-control form-control-sm" name="subject" id="subject" aria-labelledby="subject">
+                    <option>Question</option>
+                    <option>complaint</option>
+                    <option>Other</option>
+                </select>
+            </div>
+            <div class="form-group shadow-textarea  ">
+                <textarea class="form-control z-depth-1 transparent-input" id="textArea" rows="3" placeholder="Write something here..." name="comment" aria-labelledby="textArea"></textarea>
+                <p class="error"></p>
+            </div>
+            <button type="submit" class="btn btn-primary" name="submit" id="send" aria-labelledby="send">Submit</button>
+        </form>
+    </main>
 
 </body>
+
 </html>
--- verification.php
+++ verification.php
@@ -6,103 +6,107 @@ $name = $lastName = $email = $gender = $comment = $subject = $ctr = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
-  if(isset($_POST["honey"]) && $_POST["honey"] != "")
+  if(isset($_POST["submit"]))
   {
-    die();
+    if(isset($_POST["honey"]) && $_POST["honey"] != "")
+    {
+      die();
+    
+    }
+    // verify if input are empty
+    if(empty($_POST["name"]) )
+    {
+      $nameError = "Name is required";
+    }else
+    {
+      $name = verifyInput($_POST["name"]);
+      // verify if only white space and letter 
+      if(!filter_var($name,FILTER_SANITIZE_STRING) && !preg_match("/^[a-zA-z]*$/", $name)){
+        $nameError = "Only letter and wite space !";
+      }else if(!preg_match("/^[a-zA-z]*$/", $name))
+      {
+        $nameError = "Only letter and wite space !";
+      }
+    }
   
-  }
-  // verify if input are empty
-  if(empty($_POST["name"]) )
-  {
-    $nameError = "Name is required";
-  }else
-  {
-    $name = verifyInput($_POST["name"]);
-    // verify if only white space and letter 
-    if(!filter_var($name,FILTER_SANITIZE_STRING) && !preg_match("/^[a-zA-z]*$/", $name)){
-      $nameError = "Only letter and wite space !";
-    }else if(!preg_match("/^[a-zA-z]*$/", $name))
+    if(empty($_POST["lastName"]))
     {
-      $nameError = "Only letter and wite space !";
-    }
-  }
-
-  if(empty($_POST["lastName"]))
-  {
-    $lastNameError = "lastname is required";
-  }else
-  {
-     $lastName = verifyInput($_POST["lastName"]);
-     if(!filter_var($lastName,FILTER_SANITIZE_STRING)){
-      $lastNameError = "Only letter and wite space !";
-    }else if(!preg_match("/^[a-zA-z]*$/", $lastName))
+      $lastNameError = "lastname is required";
+    }else
     {
-      $nameError = "Only letter and wite space !";
+       $lastName = verifyInput($_POST["lastName"]);
+       if(!filter_var($lastName,FILTER_SANITIZE_STRING)){
+        $lastNameError = "Only letter and wite space !";
+      }else if(!preg_match("/^[a-zA-z]*$/", $lastName))
+      {
+        $nameError = "Only letter and wite space !";
+      }
     }
-  }
-
-  if(empty($_POST["email"]))
-  {
-    $emailError = "email is required";
-  }else
-  {
-    $email = verifyInput($_POST["email"]);
-    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-      $emailError = "example : blabla46@gmail.com";
-    }
-  }
-  if(empty($_POST["gender"]))
-  {
-    $genderError = "gender is required";
-  }else
-  {
-     $gender = verifyInput($_POST["gender"]);
-    if(!filter_var($gender,FILTER_SANITIZE_STRING))
-    {
-      $genderError = "gender error ";
-    }
-  }
-  if(empty($_POST["country"]))
-  {
-    $countryError = "Country is required";
-  }
-  else
-  {
-    $ctr = verifyInput(($_POST["country"]));
-    if(!filter_var($country,FILTER_SANITIZE_STRING))
-    {
-      $countryError = "country error";
-    }
-  }
-  if(empty($_POST["subject"]))
-  {
-    $subjectError = "Country is required";
-  }
-  else
-  {
-    $subject = verifyInput(($_POST["subject"]));
-    if(!filter_var($subject,FILTER_SANITIZE_STRING))
-    {
-      $subjectError = "subject error";
-    }
-  }
   
-  if(empty($_POST["comment"]))
-  {
-    $commentError = "";
-  }else
-  {
-    $comment = verifyInput($_POST["comment"]);
-    if(!filter_var($comment,FILTER_SANITIZE_STRING))
+    if(empty($_POST["email"]))
+    {
+      $emailError = "email is required";
+    }else
+    {
+      $email = verifyInput($_POST["email"]);
+      if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        $emailError = "example : blabla46@gmail.com";
+      }
+    }
+    if(empty($_POST["gender"]))
+    {
+      $genderError = "gender is required";
+    }else
+    {
+       $gender = verifyInput($_POST["gender"]);
+      if(!filter_var($gender,FILTER_SANITIZE_STRING))
+      {
+        $genderError = "gender error ";
+      }
+    }
+    if(empty($_POST["country"]))
+    {
+      $countryError = "Country is required";
+    }
+    else
+    {
+      $ctr = verifyInput(($_POST["country"]));
+      if(!filter_var($country,FILTER_SANITIZE_STRING))
+      {
+        $countryError = "country error";
+      }
+    }
+    if(empty($_POST["subject"]))
+    {
+      $subjectError = "Country is required";
+    }
+    else
+    {
+      $subject = verifyInput(($_POST["subject"]));
+      if(!filter_var($subject,FILTER_SANITIZE_STRING))
+      {
+        $subjectError = "subject error";
+      }
+    }
+    
+    if(empty($_POST["comment"]))
     {
       $commentError = "";
+    }else
+    {
+      $comment = verifyInput($_POST["comment"]);
+      if(!filter_var($comment,FILTER_SANITIZE_STRING))
+      {
+        $commentError = "";
+      }
+    }
+  
+    if(empty($nameError) && empty($lastNameError) && empty($genderError) && empty($commentError))
+    {
+      include 'sendEmail.php';
     }
   }
-
-  if(empty($nameError) && empty($lastNameError) && empty($genderError) && empty($commentError))
-  {
-    include 'sendEmail.php';
-  }
+  
 
 }
 
